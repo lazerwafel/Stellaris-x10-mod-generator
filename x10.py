23534b571c07b8db40cbb9a295762d158c359ccb
iusNiko <90134993+iusNiko@users.noreply.github.com>
2024-12-31 16:23:52 +0100

    Add Custom Factors

--- x10.py
+++ x10.py
@@ -16,6 +16,22 @@ print("PLEASE MAKE SURE THAT THE HOI4 DIRECTORY IS CORRECT! YOU CAN CHANGE IT IN
 print("If you've changed the directory, please restart the script")
 
 factor = float(input("Enter desired factor: "))
+custom_factors = {}
+
+mod_file = open(config["modifiers_list"], "r")
+for modifier in mod_file:
+    if(modifier[0] == '#'):
+        continue
+    modifiers.append(modifier.strip())
+mod_file.close()
+
+# Find any custom factors
+for modifier in modifiers:
+    custom_factor_pos = modifier.find(":")
+    if(custom_factor_pos != -1):
+        if modifier[custom_factor_pos + 1:].strip() in custom_factors:
+            continue
+        custom_factors[modifier[custom_factor_pos + 1:].strip()] = float(input("Enter custom factor for " + modifier[custom_factor_pos + 1:].strip() + ": "))
 
 if hoi4dir[len(hoi4dir) - 1] != "/" and hoi4dir[len(hoi4dir) - 1] != "\\":
     hoi4dir = hoi4dir + "/"
@@ -31,14 +47,6 @@ for dirToCopy in config["include"]:
     shutil.copytree(hoi4dir + dirToCopy, mod_root_dir + dirToCopy)
 
 print("Copied vanilla files")
-
-mod_file = open(config["modifiers_list"], "r")
-for modifier in mod_file:
-    if(modifier[0] == '#'):
-        continue
-    modifiers.append(modifier.strip())
-mod_file.close()
-
 
 # Edit copied files
 for directory, subdirectories, files in os.walk(mod_root_dir):
@@ -56,17 +64,21 @@ for directory, subdirectories, files in os.walk(mod_root_dir):
             numbers = re.findall(r"-?\d+\.?\d*", line)
             if len(numbers) != 0:
                 for modifier in modifiers:
-                    modifier_pos = line.find(modifier[1:])
+                    modifier_pos = line.find(modifier[1:modifier.find(":")])
+                    custom_factor_pos = modifier.find(":")
                     if(modifier_pos == -1):
                         continue
-                    if (modifier_pos == 0 or (modifier_pos > 0 and line[modifier_pos - 1] == " ") or (modifier_pos > 0 and line[modifier_pos - 1] == "\t")) and (line[modifier_pos + len(modifier) - 1] == " " or line[modifier_pos + len(modifier) - 1] == "="):
+                    if (modifier_pos == 0 or (modifier_pos > 0 and line[modifier_pos - 1] == " ") or (modifier_pos > 0 and line[modifier_pos - 1] == "\t")) and (line[modifier_pos + len(modifier[:modifier.find(":")]) - 1] == " " or line[modifier_pos + len(modifier[:modifier.find(":")]) - 1] == "="):
                         
                         if float(numbers[0]) < 0 and modifier[0] == '+':
                             continue
                         if float(numbers[0]) > 0 and modifier[0] == '-':
                             continue
-                            
-                        new_line = line.replace(str(numbers[0]), "{:.3f}".format(float(numbers[0]) * factor))
+                        
+                        current_factor = factor
+                        if custom_factor_pos != -1:
+                            current_factor = custom_factors[modifier[custom_factor_pos + 1:].strip()]
+                        new_line = line.replace(str(numbers[0]), "{:.3f}".format(float(numbers[0]) * current_factor))
                         break
             new_content += new_line
         edited_file.close()
